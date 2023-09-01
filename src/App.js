9e4c8a4d9e219d064aec43db54f12ad583e0cf16
Dipra <dipradas5940@gmail.com>
2023-09-01 12:31:45 +0600

    Project Attached

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/routes";
 
-const App = () => {
+
+function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <RouterProvider router={routes} />
+    </>
   );
-};
+}
 
 export default App;
